eed0efbfb0d4a10976d7343034368069ba59fcf8
Thom2503 <thomveldhuis03@gmail.com>
2023-02-07 10:45:04 +0100

    [week:1] Eerste 3 af

---
+++ week1/lesson/W01.1.C02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Program;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("What is your age?");
+		int iAge = Convert.ToInt32(Console.ReadLine());
+
+		Console.WriteLine($"You are {iAge}. That's old enough to program!");
+		Console.WriteLine($"Last year you were {iAge - 1}");
+		Console.WriteLine($"Next year you will be {iAge + 1}");
+		Console.WriteLine($"At double your age you will be {iAge * 2}");
+		Console.WriteLine($"Next year, double your age will be {(iAge+1) * 2}");
+
+		Console.WriteLine($"Half your age is {(double)iAge / 2.0}");
+		Console.WriteLine($"Half your age (rounded down) is {iAge / 2}");
+		Console.WriteLine($"The last digit of your age is {iAge % 10}");
+	}
+}
---
+++ week1/lesson/W01.1.C03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Program;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		Console.Write("What is the temperature in Celsius?\n>");
+		double dbCelcius = Convert.ToDouble(Console.ReadLine());
+
+		double dbFarhenheit = dbCelcius * 1.8 + 32;
+		Console.WriteLine($"{dbCelcius} C = {dbFarhenheit} F");
+		Console.WriteLine($"Rounded down that is {Math.Floor(dbFarhenheit)} F");
+	}
+}
