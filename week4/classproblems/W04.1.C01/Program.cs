aee75ce1b03e29999886cf3edf7cad332488489c
Thom2503 <thomveldhuis03@gmail.com>
2023-02-22 19:41:28 +0100

    [week:4] begonnen aan week 4

---
+++ week4/classproblems/W04.1.C01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Program;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		string fileContents = File.ReadAllText("MyTextFile.txt");
+		Console.Write(fileContents);
+	}
+}
---
+++ week4/classproblems/W04.1.C02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Program;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		File.AppendAllText("MyTextFile.txt", "\nAdded Line.");
+		string fileContents = File.ReadAllText("MyTextFile.txt");
+		Console.Write(fileContents);
+	}
+}
