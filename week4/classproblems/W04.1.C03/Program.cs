d24c5d20f6a6fb7e7b07039a086297c6acfc4cf6
Thom2503 <thomveldhuis03@gmail.com>
2023-03-05 20:36:29 +0100

    [week:4] toch maar uitleg volgende week volgen

---
+++ week4/classproblems/W04.1.C03/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		StreamReader reader = new StreamReader("Cars.json");
+		string File2JSON = reader.ReadToEnd();
+		List<Car> lCars = JsonConvert.DeserializeObject<List<Car>>(File2JSON)!;
+		reader.Close();
+		reader.Dispose();
+
+		foreach (Car car in lCars)
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				car.Drive();
+			}
+		}
+
+		StreamWriter writer = new StreamWriter("Cars.json");
+		string List2JSON = JsonConvert.SerializeObject(lCars);
+		writer.Write(List2JSON);
+		writer.Close();
+		writer.Dispose();
+	}
+}
---
+++ week4/classproblems/W04.1.C05/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace Program;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		StreamReader reader = new StreamReader("People.json");
+		string File2JSON = reader.ReadToEnd();
+		List<Person> lPersons = JsonConvert.DeserializeObject<list<Person>>(File2JSON)!;
+		reader.Close();
+		reader.Dispose();
+
+		foreach (Person person in lPersons)
+			foreach (Car personCar in person.OwnedCars)
+				for (int i = 0; i < 5; i++)
+					personCar.Drive();
+
+		StreamWriter writer = new StreamWriter("People.json");
+		string List2JSON = JsonConvert.SerializeObject(lPersons);
+		writer.Write(List2JSON);
+		writer.Close();
+		writer.Dispose();
+	}
+}
